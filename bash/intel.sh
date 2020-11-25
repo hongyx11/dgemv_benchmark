212b9039c672a18836df6f70784f6f169e374ddb
yuxihong <yuxi.hong@kaust.edu.sa>
2020-11-25 08:38:31 +0100

    update driver

--- bash/intel.sh
+++ bash/intel.sh
@@ -11,16 +11,16 @@ fi
 # no transpose 
 ./bin/intelsingle fixed 5000 10000 100 ${CPUTYPE} 
 ./bin/intelsingle fixed 5000 25000 100 ${CPUTYPE} 
-# ./bin/intelsingle fixed 5000 20000 100 ${CPUTYPE} 
-# ./bin/intelsingle fixed 8000 80000 100 ${CPUTYPE} 
+./bin/intelsingle fixed 5000 20000 100 ${CPUTYPE} 
+./bin/intelsingle fixed 8000 80000 100 ${CPUTYPE} 
 # ./bin/intelsingle fixed 35000 70000 100 ${CPUTYPE} 
 
-# # no transpose 
-# ./bin/inteldouble fixed 5000 10000 100 ${CPUTYPE} 
-# ./bin/inteldouble fixed 5000 25000 100 ${CPUTYPE} 
-# ./bin/inteldouble fixed 5000 20000 100 ${CPUTYPE} 
-# ./bin/inteldouble fixed 8000 80000 100 ${CPUTYPE} 
-# ./bin/inteldouble fixed 35000 700000 100 ${CPUTYPE} 
+# no transpose 
+./bin/inteldouble fixed 5000 10000 100 ${CPUTYPE} 
+./bin/inteldouble fixed 5000 25000 100 ${CPUTYPE} 
+./bin/inteldouble fixed 5000 20000 100 ${CPUTYPE} 
+./bin/inteldouble fixed 8000 80000 100 ${CPUTYPE} 
+# ./bin/inteldouble fixed 35000 70000 100 ${CPUTYPE} 
 
 
 # # transpose
@@ -28,7 +28,7 @@ fi
 # ./bin/intelsingle_transpose fixed 25000 5000 100 ${CPUTYPE} 
 # ./bin/intelsingle_transpose fixed 20000 5000 100 ${CPUTYPE} 
 # ./bin/intelsingle_transpose fixed 80000 8000 100 ${CPUTYPE} 
-# ./bin/intelsingle_transpose fixed 700000 35000 100 ${CPUTYPE} 
+# ./bin/intelsingle_transpose fixed 70000 35000 100 ${CPUTYPE} 
 
 
 # # transpose
@@ -36,4 +36,4 @@ fi
 # ./bin/inteldouble_transpose fixed 25000 5000 100 ${CPUTYPE} 
 # ./bin/inteldouble_transpose fixed 20000 5000 100 ${CPUTYPE} 
 # ./bin/inteldouble_transpose fixed 80000 8000 100 ${CPUTYPE} 
-# ./bin/inteldouble_transpose fixed 700000 35000 100 ${CPUTYPE} 
+# ./bin/inteldouble_transpose fixed 70000 35000 100 ${CPUTYPE} 
