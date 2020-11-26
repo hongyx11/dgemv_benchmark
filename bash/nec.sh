765415e4522aa2fcdefa4cec8d96d982beb2a082
yuxihong <yuxi.hong@kaust.edu.sa>
2020-11-26 09:14:28 +0100

    add scexao dim

--- bash/nec.sh
+++ bash/nec.sh
@@ -9,16 +9,18 @@ exit
 fi
 
 # no transpose 
-./bin/necsingle fixed 5000 10000 1000 ${CPUTYPE} 
-./bin/necsingle fixed 5000 20000 1000 ${CPUTYPE} 
-./bin/necsingle fixed 5000 25000 1000 ${CPUTYPE} 
-./bin/necsingle fixed 8000 80000 1000 ${CPUTYPE} 
-./bin/necsingle fixed 35000 70000 1000 ${CPUTYPE} 
+# ./bin/necsingle fixed 5000 10000 1000 ${CPUTYPE} 
+# ./bin/necsingle fixed 5000 20000 1000 ${CPUTYPE} 
+./bin/necsingle fixed 2000 14000 1000 ${CPUTYPE} 
+# ./bin/necsingle fixed 5000 25000 1000 ${CPUTYPE} 
+# ./bin/necsingle fixed 8000 80000 1000 ${CPUTYPE} 
+# ./bin/necsingle fixed 35000 70000 1000 ${CPUTYPE} 
 
 # no transpose 
-./bin/necdouble fixed 5000 10000 1000 ${CPUTYPE} 
-./bin/necdouble fixed 5000 20000 1000 ${CPUTYPE} 
-./bin/necdouble fixed 5000 25000 1000 ${CPUTYPE} 
-./bin/necdouble fixed 8000 80000 1000 ${CPUTYPE} 
-./bin/necdouble fixed 35000 70000 1000 ${CPUTYPE} 
+# ./bin/necdouble fixed 5000 10000 1000 ${CPUTYPE} 
+# ./bin/necdouble fixed 5000 20000 1000 ${CPUTYPE} 
+./bin/necdouble fixed 2000 14000 1000 ${CPUTYPE} 
+# ./bin/necdouble fixed 5000 25000 1000 ${CPUTYPE} 
+# ./bin/necdouble fixed 8000 80000 1000 ${CPUTYPE} 
+# ./bin/necdouble fixed 35000 70000 1000 ${CPUTYPE} 
 
