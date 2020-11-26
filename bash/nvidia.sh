ebdd97a220d1835dcab446d1b9e16e578373d9b4
Hatem Ltaief <Hatem.Ltaief@kaust.edu.sa>
2020-11-26 04:11:29 -0800

    bench for multiple GPUs

--- bash/nvidia.sh
+++ bash/nvidia.sh
@@ -16,18 +16,88 @@ echo "CUDA_VISIBLE_DEVICES not set. please try agin."
 exit
 fi
 
-# no transpose 
-# ./bin/nvidiasingle fixed 5000 10000 1000 ${GPUTYPE} 
-# ./bin/nvidiasingle fixed 5000 20000 1000 ${GPUTYPE} 
-./bin/nvidiasingle fixed 2000 14000 1000 ${GPUTYPE} 
-# ./bin/nvidiasingle fixed 5000 25000 1000 ${GPUTYPE} 
-# ./bin/nvidiasingle fixed 8000 80000 1000 ${GPUTYPE} 
-# ./bin/nvidiasingle fixed 35000 70000 1000 ${GPUTYPE} 
+###########################################################################
 
+# MICADO [1] instrument
 # no transpose 
-# ./bin/nvidiadouble fixed 5000 10000 1000 ${GPUTYPE} 
-# ./bin/nvidiadouble fixed 5000 20000 1000 ${GPUTYPE} 
-./bin/nvidiadouble fixed 2000 14000 1000 ${GPUTYPE} 
-# ./bin/nvidiadouble fixed 5000 25000 1000 ${GPUTYPE} 
-# ./bin/nvidiadouble fixed 8000 80000 1000 ${GPUTYPE} 
-# ./bin/nvidiadouble fixed 35000 70000 1000 ${GPUTYPE} 
+#./bin/nvidiasingle fixed 5000 10000 1000 ${GPUTYPE}  # Dimension for 1 GPU
+./bin/nvidiasingle fixed 2500 10000 1000 ${GPUTYPE}  # Dimension for 2 GPUs
+./bin/nvidiasingle fixed 1250 10000 1000 ${GPUTYPE}  # Dimension for 4 GPUs
+./bin/nvidiasingle fixed 625 10000 1000 ${GPUTYPE}  # Dimension for 8 GPUs
+
+#./bin/nvidiadouble fixed 5000 10000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiadouble fixed 2500 10000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiadouble fixed 1250 10000 1000 ${GPUTYPE} # Dimension for 4 GPUs 
+./bin/nvidiadouble fixed 625 10000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+###########################################################################
+
+# MAVIS instrument
+# no transpose 
+#./bin/nvidiasingle fixed 5000 20000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiasingle fixed 2500 20000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiasingle fixed 1250 20000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiasingle fixed 625 20000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+#./bin/nvidiadouble fixed 5000 20000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiadouble fixed 2500 20000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiadouble fixed 1250 20000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiadouble fixed 625 20000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+###########################################################################
+
+# SCEXAO instrument
+# no transpose 
+#./bin/nvidiasingle fixed 2000 14000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiasingle fixed 1000 14000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiasingle fixed 500 14000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiasingle fixed 200 14000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+#./bin/nvidiadouble fixed 2000 14000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiadouble fixed 1000 14000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiadouble fixed 500 14000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiadouble fixed 200 14000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+###########################################################################
+
+# MICADO [2] instrument
+# no transpose 
+#./bin/nvidiasingle fixed 5000 25000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiasingle fixed 2500 25000 1000 ${GPUTYPE}  # Dimension for 2 GPUs
+./bin/nvidiasingle fixed 1250 25000 1000 ${GPUTYPE}  # Dimension for 4 GPUs
+./bin/nvidiasingle fixed 625 25000 1000 ${GPUTYPE}  # Dimension for 8 GPUs
+
+#./bin/nvidiadouble fixed 5000 25000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiadouble fixed 2500 25000 1000 ${GPUTYPE}  # Dimension for 2 GPUs
+./bin/nvidiadouble fixed 1250 25000 1000 ${GPUTYPE}  # Dimension for 4 GPUs
+./bin/nvidiadouble fixed 625 25000 1000 ${GPUTYPE}  # Dimension for 8 GPUs
+
+###########################################################################
+
+# MAORY instrument
+# no transpose 
+#./bin/nvidiasingle fixed 8000 80000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiasingle fixed 4000 80000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiasingle fixed 2000 80000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiasingle fixed 1000 80000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+#./bin/nvidiadouble fixed 8000 80000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiadouble fixed 4000 80000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiadouble fixed 2000 80000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiadouble fixed 1000 80000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+###########################################################################
+
+# EPICS instrument
+# no transpose 
+#./bin/nvidiasingle fixed 35000 70000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiasingle fixed 17500 70000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiasingle fixed 8750 70000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiasingle fixed 4375 70000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+#./bin/nvidiadouble fixed 35000 70000 1000 ${GPUTYPE} # Dimension for 1 GPU
+./bin/nvidiadouble fixed 17500 70000 1000 ${GPUTYPE} # Dimension for 2 GPUs
+./bin/nvidiadouble fixed 8750 70000 1000 ${GPUTYPE} # Dimension for 4 GPUs
+./bin/nvidiadouble fixed 4375 70000 1000 ${GPUTYPE} # Dimension for 8 GPUs
+
+###########################################################################
